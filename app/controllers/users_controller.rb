2b64fba8655a404748437510522d1bb8c29b7fc7
sophieluo <sophie@groobusiness.com>
2016-12-18 14:26:37 -0800

    build fileupload in a separate repo

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.create( user_params )
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to('/users')
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  # Use strong_parameters for attribute whitelisting
+  # Be sure to update your create() and update() controller methods.
+
+  def user_params
+    params.require(:user).permit(:avatar, :name)
+  end
+
+end
